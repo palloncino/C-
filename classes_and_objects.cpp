49bed7561b0b5498edc6caa81e9a584f3af16319
palloncino <antonio.guiotto.dev@gmail.com>
2022-03-15 17:17:39 +0100

    using constructor

--- classes_and_objects.cpp
+++ classes_and_objects.cpp
@@ -6,13 +6,33 @@ class Person {
     int age;
     string name;
     void sayMyName() {
-      cout << "Hi, my name is " + name;
+      cout << "Hi, my name is " << name << "\n";
+    }
+};
+
+class Animal {
+  public:
+    string type;
+    int weight;
+    Animal(string t, int w) {
+      type = t;
+      weight = w;
+    }
+    void getInfo() {
+      cout << "This is a " << type << " and weight around " << std::to_string(weight) << "kg." << "\n";
     }
 };
 
 int main() {
-  Person Antonio;
-  Antonio.name = "Antonio";
-  Antonio.sayMyName();
+  
+  // without constructor
+  Person antonio;
+  antonio.name = "Antonio";
+  antonio.sayMyName();
+
+  // with constructor
+  Animal cat("mammal", 5);
+  cat.getInfo();
+
   return 0;
 };
